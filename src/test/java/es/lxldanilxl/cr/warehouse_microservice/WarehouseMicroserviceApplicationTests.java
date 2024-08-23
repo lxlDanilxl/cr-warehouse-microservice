d1b99e58a869504f6a94049b46733fbc3957372a
Rogelio Daniel Franco Martínez <contacto@lxldanilxl.es>
2024-08-23 21:27:09 +0200

    Initial commit

---
+++ src/test/java/es/lxldanilxl/cr/warehouse_microservice/WarehouseMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package es.lxldanilxl.cr.warehouse_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
